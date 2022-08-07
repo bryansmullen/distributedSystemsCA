3a057ecbe57720080e11ae706ae05dc75c48b01c
Bryan Mullen <bryan@bryanmullen.me>
2022-08-07 17:15:13 +0100

    Implement Feed Service Method Skeletons

--- src/main/java/com/bryanmullen/feed/server/FeedServiceImpl.java
+++ src/main/java/com/bryanmullen/feed/server/FeedServiceImpl.java
@@ -1,20 +1,36 @@
 package com.bryanmullen.feed.server;
 
-import com.bryanmullen.feedService.FeedRequest;
-import com.bryanmullen.feedService.FeedResponse;
-import com.bryanmullen.feedService.FeedServiceGrpc;
+import com.bryanmullen.feedService.*;
 import io.grpc.stub.StreamObserver;
 
 
 public class FeedServiceImpl extends FeedServiceGrpc.FeedServiceImplBase {
     @Override
-    public void feedMethod(FeedRequest request,
-                           StreamObserver<FeedResponse> responseStreamObserver) {
-        System.out.println("receiving dummy request");
-        FeedResponse reply =
-                FeedResponse.newBuilder()
-                        .setMessage("Hello " + request.getName())
-                        .build();
+    public void addToFeedAvailable(AddToFeedRequest request,
+                                   StreamObserver<AddToFeedResponse> responseStreamObserver){
+        System.out.println("Receiving Add To Feed Available Request");
+        var reply = AddToFeedResponse.newBuilder()
+                .build();
+        responseStreamObserver.onNext(reply);
+        responseStreamObserver.onCompleted();
+    }
+
+    @Override
+    public void currentWaterAvailable(CurrentWaterRequest request,
+                                      StreamObserver<CurrentWaterResponse> responseStreamObserver){
+        System.out.println("Receiving Current Water Available Request");
+        var reply = CurrentWaterResponse.newBuilder()
+                .build();
+        responseStreamObserver.onNext(reply);
+        responseStreamObserver.onCompleted();
+    }
+
+    @Override
+    public void feedConsumption(FeedConsumptionRequest request,
+                                   StreamObserver<FeedConsumptionResponse> responseStreamObserver){
+        System.out.println("Receiving Feed Consumption Request");
+        var reply = FeedConsumptionResponse.newBuilder()
+                .build();
         responseStreamObserver.onNext(reply);
         responseStreamObserver.onCompleted();
     }
