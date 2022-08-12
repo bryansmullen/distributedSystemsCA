749a2ec3c913adc031d3dce68442dee63301fe78
Bryan Mullen <bryan@bryanmullen.io>
2022-08-12 15:06:35 +0100

    Implement Basic CLI Client For addToFeedAvailable Bi Directional Streaming Request

--- src/main/java/com/bryanmullen/services/feed/client/cli/FeedClient.java
+++ src/main/java/com/bryanmullen/services/feed/client/cli/FeedClient.java
@@ -1,14 +1,16 @@
 package com.bryanmullen.services.feed.client.cli;
 
-import com.bryanmullen.feedService.CurrentWaterRequest;
-import com.bryanmullen.feedService.FeedConsumptionRequest;
-import com.bryanmullen.feedService.FeedServiceGrpc;
+import com.bryanmullen.feedService.*;
 import com.bryanmullen.services.shared.ClientBase;
 import com.google.protobuf.Timestamp;
+import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class FeedClient extends ClientBase {
     Logger logger = LoggerFactory.getLogger(FeedClient.class); //
@@ -19,10 +21,65 @@ public class FeedClient extends ClientBase {
         super(propertiesFilePath);
     }
 
-    public void addToFeedAvailable() {
-        System.out.println("Starting to do Add To Feed Available method...");
+    /**
+     * This method should allow the client to open a delivery chute for feed for the animals. Further to this it should
+     * log the current weight before and after delivery in order to log the amount consumed.This method will be
+     * implemented using Bi Directional Streaming.
+     */
+    public void addToFeedAvailable() throws InterruptedException {
+        // log the start of the call
+        logger.info("Starting to do Add To Feed Available method...");
+
+        var stub = FeedServiceGrpc.newStub(getChannel());
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        var streamObserver = stub.addToFeedAvailable(new StreamObserver<>() {
+            @Override
+            public void onNext(AddToFeedResponse response) {
+                logger.info("Received response: " + response);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                latch.countDown();
+            }
+        });
+        Arrays.asList(
+                new feedMassToAdd(1, "Adam"),
+                new feedMassToAdd(2, "Eve"),
+                new feedMassToAdd(3, "Jack")
+        ).forEach(requestData -> {
+                    streamObserver.onNext(
+                            AddToFeedRequest.newBuilder()
+                                    .setFeedMassToAdd(requestData.feedMassToAdd)
+                                    .setAddedBy(requestData.addedBy)
+                                    .build());
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+
+        streamObserver.onCompleted();
+        //noinspection ResultOfMethodCallIgnored
+        latch.await(5, TimeUnit.SECONDS);
     }
 
+    private record feedMassToAdd(int feedMassToAdd, String addedBy) {
+    }
+
+    /**
+     * This method should simply return the current water available by means of a weight/volume sensor, for ensuring
+     * there is adequate water available .This method will be implemented using Server Streaming.
+     */
     public void currentWaterAvailable() {
         // log the start of the call
         logger.info("Starting to do Current Water Available method...");
@@ -44,6 +101,11 @@ public class FeedClient extends ClientBase {
 
     }
 
+    /**
+     * This method should return a report outlining the feed consumed in a particular time period. This could be used
+     * either for predicting supply levels or to monitor animal health.This method will be implemented using Unary
+     * Method.
+     */
     public void feedConsumption() {
         // log the start of the call
         logger.info("Starting to do Milk Production method...");
--- src/main/java/com/bryanmullen/services/feed/client/cli/FeedClientApp.java
+++ src/main/java/com/bryanmullen/services/feed/client/cli/FeedClientApp.java
@@ -3,7 +3,7 @@ package com.bryanmullen.services.feed.client.cli;
 import java.io.IOException;
 
 public class FeedClientApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         var feedClient = new FeedClient("src/main/resources/feed.properties");
 
         feedClient.getService();
