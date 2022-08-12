5f94cb4a504c493cf086fca360f866e214d2708f
Bryan Mullen <bryan@bryanmullen.io>
2022-08-12 11:56:48 +0100

    Implmenet Basic CLI Client For milkProduction Unary Request

--- src/main/java/com/bryanmullen/services/milking/client/cli/MilkingClient.java
+++ src/main/java/com/bryanmullen/services/milking/client/cli/MilkingClient.java
@@ -1,25 +1,60 @@
 package com.bryanmullen.services.milking.client.cli;
+
+import com.bryanmullen.milkingService.MilkProductionRequest;
+import com.bryanmullen.milkingService.MilkingServiceGrpc;
 import com.bryanmullen.services.shared.ClientBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 public class MilkingClient extends ClientBase {
+    Logger logger = LoggerFactory.getLogger(MilkingClient.class); //
+    // Logger for this class so we can log messages to the console.
+
     public MilkingClient(String propertiesFilePath) throws IOException {
         super(propertiesFilePath);
     }
 
+    /**
+     * This method should be able to represent the total milk currently collected in the collection unit relative to the
+     * capacity, to give an indication of when the unit is full.This method will be implemented using Server Streaming.
+     */
     public void milkCollection() {
         // inform the user of the start of the call
         System.out.println("Starting to do Milk Collection method...");
         closeChannel();
     }
 
+    /**
+     * This method should be able to log the milk produced by the current milking session against a particular cow,
+     * based on the ID of the current cow. It should calculate the volume of milk by comparing the current total
+     * collection against the last logged value and calculating the difference.This method will be implemented using
+     * Unary Method.
+     */
     public void milkProduction() {
-        // inform the user of the start of the call
-        System.out.println("Starting to do Milk Production method...");
+        // log the start of the call
+        logger.info("Starting to do Milk Production method...");
+
+        // create the client stub for the service
+        var stub = MilkingServiceGrpc.newBlockingStub(getChannel());
+
+        // get the response from the server by calling the service with a new request
+        var response =
+                stub.milkProduction(MilkProductionRequest.newBuilder().setCheckedBy("Bryan").build());
+
+        // print the response to the console
+        System.out.println("Milk Production Response: " + response);
+
+        // close the channel
         super.closeChannel();
 
     }
 
+    /**
+     * This method should be capable of returning the ID of the current cow being milked. This method will be
+     * implemented using Server Streaming.
+     */
     public void milkCurrentCow() {
         // inform the user of the start of the call
         System.out.println("Starting to do Milk Current Cow method...");
