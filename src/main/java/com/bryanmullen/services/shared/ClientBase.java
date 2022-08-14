82072608d5740464473b8d5d86f23f91368b4448
Bryan Mullen <bryan@bryanmullen.io>
2022-08-14 12:21:58 +0100

    Add Comments To Abstract Base Classes

--- src/main/java/com/bryanmullen/services/shared/ClientBase.java
+++ src/main/java/com/bryanmullen/services/shared/ClientBase.java
@@ -1,10 +1,13 @@
 package com.bryanmullen.services.shared;
 
+import com.bryanmullen.interceptors.ClientInterceptor;
 import com.bryanmullen.mdns.PropertiesReader;
 import com.bryanmullen.mdns.ServiceDiscovery;
 import io.grpc.ManagedChannel;
 import io.grpc.netty.GrpcSslContexts;
 import io.grpc.netty.NettyChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jmdns.ServiceInfo;
 import javax.net.ssl.SSLException;
@@ -14,59 +17,83 @@ import java.util.Objects;
 import java.util.Properties;
 
 public abstract class ClientBase {
+    // instance variables
+
+    // logger for this class
+    Logger logger = LoggerFactory.getLogger(ClientInterceptor.class);
     Properties properties;
     ManagedChannel channel;
     ServiceInfo serviceInfo;
 
+    // constructor
     public ClientBase(String propertiesFilePath) throws IOException {
-        properties = PropertiesReader.getProperties(propertiesFilePath);
+        properties = PropertiesReader.getProperties(propertiesFilePath); // Get the properties from the properties file.
     }
 
+    // methods
+
+    /**
+     * getService - This service will get the service type from the properties file. It will then run the service
+     * discovery to find the service information before creating the channel using the service information.
+     *
+     */
     public void getService() throws SSLException {
         // find service on the network
         String SERVICE_TYPE = properties.getProperty("service_type");
 
+        // discover the service
         serviceInfo = ServiceDiscovery.run(SERVICE_TYPE);
 
+        // in the case that the service is not found, log the error.
         if (serviceInfo == null) {
-            System.out.println("No service found on the network");
+            logger.error("Service not found.");
         } else {
+            // pull data from the service info
             String HOST = serviceInfo.getHostAddresses()[0];
             int PORT = serviceInfo.getPort();
 
             // log that service has been found
-            System.out.println("Found service at " + HOST + ":" + PORT);
+            logger.info("Service found at " + HOST + ":" + PORT);
 
             // create the channel
             setChannel(HOST, PORT);
         }
     }
 
+    /**
+     * closeChannel - This method will close the channel which allows for graceful shutdown of the channel.
+     */
     public void closeChannel() {
-        System.out.println("Closing channel...");
-        channel.shutdown();
-        System.out.println("Channel closed");
+        logger.info("Closing channel..."); // Log that the channel is being closed.
+        channel.shutdown(); // Close the channel.
+        logger.info("Channel closed."); // Log that the channel has been closed.
     }
 
+    // setters and getters
     public ManagedChannel getChannel() {
         return channel;
     }
 
     private void setChannel(String host, int port) throws SSLException {
-
-        channel = NettyChannelBuilder.forAddress("localhost", port)
-                .sslContext(GrpcSslContexts
-                        .forClient()
+        // create the channel using NettyChannelBuilder - this allows us to use SSL
+        channel = NettyChannelBuilder.forAddress("localhost", port) // Set the host and port.
+                .sslContext(GrpcSslContexts // Set the SSL context.
+                        .forClient() // For the client.
                         .trustManager(getFile()) // public key
-                        .build())
-                .build();
+                        .build()) // build the SSL context.
+                .build(); // build the channel.
     }
 
 
+    /**
+     * getFile - This method will get the file from the path.
+     *
+     * @return The file.
+     */
     private static File getFile() {
         return new File(Objects.requireNonNull(ClientBase
                         .class
-                        .getResource("/my-public-key-cert.pem"))
+                        .getResource("/my-public-key-cert.pem")) // Get the file from the path. - this is the public key.
                 .getFile());
     }
 
--- src/main/java/com/bryanmullen/services/shared/ServerBase.java
+++ src/main/java/com/bryanmullen/services/shared/ServerBase.java
@@ -1,9 +1,12 @@
 package com.bryanmullen.services.shared;
 
+import com.bryanmullen.interceptors.ClientInterceptor;
 import com.bryanmullen.mdns.PropertiesReader;
 import com.bryanmullen.mdns.ServiceRegistration;
 import io.grpc.BindableService;
 import io.grpc.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import javax.jmdns.JmDNS;
@@ -11,79 +14,105 @@ import javax.jmdns.ServiceInfo;
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * ServiceBase is the base class for the services. This class will be used to create the implementation of the server
+ * classes.
+ */
 public abstract class ServerBase {
     // instance variables
+
+    // logger for this class
+    Logger logger = LoggerFactory.getLogger(ClientInterceptor.class);
     Properties properties;
     BindableService serviceImpl;
     String serverName;
 
-
     // constructor
     public ServerBase(String propertiesFilePath, BindableService bindableService) throws IOException {
+        // get the properties from the properties file.
         properties = PropertiesReader.getProperties(propertiesFilePath);
+
+        // set the server name from the properties file.
         setServerName(properties.getProperty("service_name"));
+
+        // set the service implementation to the bindable service passed in.
         serviceImpl = bindableService;
     }
 
 
     // methods
+
+    /**
+     * run - This method will run the server. It will be called by the implementing class.
+     *
+     * @param server - the server to run.
+     */
     public void run(Server server) throws InterruptedException {
-        // constants
-        startServer(server);
 
-        var serviceRegistration = new ServiceRegistration(properties);
-
-            serviceRegistration.register();
-
-
-        serverListen(server, serviceRegistration.getJmDNS(), serviceRegistration.getServiceInfo());
-    }
-
-    private void startServer(Server server) {
 
         // start the server
         try {
             server.start();
         } catch (IOException e) {
-            System.out.println("Failed to start server: " + e.getMessage());
+            logger.error("Server failed to start." + e.getMessage());
         }
 
-        System.out.println("Server started on port " + properties.getProperty("service_port"));
+        // log that the server has started
+        logger.info("Server started on port " + properties.getProperty("port"));
+
+        // register the service with the network
+        var serviceRegistration = new ServiceRegistration(properties);
+        serviceRegistration.register();
+
+        // listen for requests
+        serverListen(server, serviceRegistration.getJmDNS(), serviceRegistration.getServiceInfo());
     }
 
+    /**
+     * serverListen - This method will listen for requests on the server.
+     *
+     * @param server - the server to listen on.
+     * @param mdns - the jmdns instance to use.
+     * @param serviceInfo - the service information to use.
+     */
     private void serverListen(io.grpc.Server server, JmDNS mdns,
                               ServiceInfo serviceInfo) {
-        // shutdown hook to stop the server
+        // shutdown hook to stop the server gracefully.
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("Shutting down " + serverName + "...");
+            // log that the server is shutting down
+            logger.info("Shutting down gRPC server " + serverName + "..."); // log that the server is shutting down
+
             // unregister the service
-            mdns.unregisterService(serviceInfo);
-            System.out.println("Service Unregistered");
+            logger.info("Unregistering service " + serviceInfo.getName() + "..."); // log that the service is unregistering
+            mdns.unregisterService(serviceInfo); // unregister the service
+            logger.info("Service unregistered."); // log that the service has been unregistered
 
             // close the jMDNS instance
             try {
-                mdns.close();
-
-                System.out.println("jMDNS closed");
+                mdns.close(); // close the jMDNS instance
+                logger.info("jMDNS closed."); // log that the jMDNS instance has been closed.
             } catch (IOException e) {
+                // log that the jMDNS instance failed to close.
+                logger.error("jMDNS failed to close." + e.getMessage());
                 throw new RuntimeException(e);
             }
 
             // shutdown the server
-            server.shutdown();
-            System.out.println("Server shut down");
+            server.shutdown(); // shutdown the server
+            logger.info("Server shutdown."); // log that the server has been shutdown.
         }));
 
         // block until the server is terminated
 
         try {
             // inform the user that the server is ready
-            System.out.println(serverName +
-                    " Listening for Requests...");
+            logger.info("Server " + serverName + " is listening for Requests..."); // log that the server is ready
 
+            // block until the server is terminated
             server.awaitTermination();
         } catch (InterruptedException e) {
-            System.out.println("Failed to await termination: " + e.getMessage());
+            // log that the server was interrupted
+            logger.error("Server " + serverName + " was interrupted." + e.getMessage());
         }
     }
 
